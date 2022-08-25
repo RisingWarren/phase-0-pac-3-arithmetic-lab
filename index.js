f784654493907a99f848757adb7f0ce198523cff
Warren Santiago <wsantiago9623@gmail.com>
2022-08-24 20:29:38 -0400

    commit tree 80dba81b48b1a41346584b03a00fc8eab64980aa

---
+++ index.js
@@ -0,0 +1,66 @@
+function add(a,b){
+    return a + b
+
+}
+
+function subtract(a, b){
+    return a - b
+
+}
+
+function multiply(a, b){
+    return a * b
+
+}
+
+function divide(a, b){
+    return a / b
+
+}
+
+function increment(a){
+    return (a + 1)
+
+}
+
+function decrement(a){
+    return (a - 1)
+
+}
+
+function makeInt(a,b=10){
+    parseInt("2", 10);
+    
+
+
+    return parseInt(a,b)
+
+    
+
+
+
+}
+
+function preserveDecimal(a){
+    parseFloat ("a");
+    parseFloat ('n');
+    return parseFloat(a);
+
+
+
+
+}
+
+
+add(a, b)
+
+subtract(a, b)
+
+multiply(a, b)
+
+divide(a, b)
+
+makeInt(a, b)
+
+
+
